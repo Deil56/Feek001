b9d1351220662ee29f2f70af45477898d7993e1e
Yaroslav <deil56rus@mail.ru>
2023-04-22 17:53:48 +0500

    изменения

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -9,9 +9,9 @@ int num_B = Convert.ToInt32(Console.ReadLine());
 
 if (num_A > num_B)
 {
-    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+    Console.WriteLine($"Первое число {num_A} больше чем второе {num_B}");
 }
 else
 {
-    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+    Console.WriteLine($"Второе число  {num_B} больше чем первое {num_A}");
 }
