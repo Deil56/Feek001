c05908b273c4eb5be595c317d89fb7c1c5a65604
Yaroslav <deil56rus@mail.ru>
2023-04-24 22:04:09 +0500

    изменения

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -5,7 +5,6 @@ Console.WriteLine("Введите 3 числа:");
 int num_1 = Convert.ToInt32(Console.ReadLine());
 int num_2 = Convert.ToInt32(Console.ReadLine());
 int num_3 = Convert.ToInt32(Console.ReadLine());
-
 int max = num_1;
 
 if (num_2 > max)
