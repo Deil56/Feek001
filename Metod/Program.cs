63575bcadf58005edcc6f82584761b07170e9f03
Yaroslav <deil56rus@mail.ru>
2023-04-25 00:46:03 +0500

    изменили файл

--- Metod/Program.cs
+++ Metod/Program.cs
@@ -9,10 +9,26 @@
 int number = new Random().Next(10, 100); // (10, 99 + 1)
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+
+// if(firstDigit > secondDigit) 
+// {
+//     Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+// }
+
+//  Тернарный оператор
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
 int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
 
-int MaxDigit(int num)
+int MaxDigit(int num) // num = number
 {
     int firstDigit = num / 10;
     int secondDigit = num % 10;
---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого
+числа.
+456 -> 46
+782 -> 72
+918 -> 98
+ */
+
+int number = new Random().Next(100, 1000);
+int finaleNumber = DeleteSecond(number);
+Console.WriteLine($"трехзначное число {number} -> с удаленной второй цифрой {finaleNumber}");
+
+ int DeleteSecond(int num)
+{
+    int firstNumber = num /100;
+    int lastNumber = num %10;
+    int finaleNumber = firstNumber * 10 + lastNumber;
+    return finaleNumber;
+}
