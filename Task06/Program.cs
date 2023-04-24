8134977e86e61bb20d7aef29792265844359d84e
Yaroslav <deil56rus@mail.ru>
2023-04-24 22:57:40 +0500

    изменения

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -4,11 +4,9 @@
 Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a % 2 == 1)
+if (a % 2 == 0)
 {
-    Console.WriteLine($"Число {a} является: НЕЧЁТНЫМ");
+    Console.WriteLine($"Число {a} является: является четным");
 }
-else
-{
-    Console.WriteLine($"Число {a} является: ЧЁТНЫМ");
-}
+
+else Console.WriteLine($"Число {a} является: не является четным");
